0472945b12d16e82251a206b10c1b5e0035d0c14
elloyy <161834075+juzieatulalifah@users.noreply.github.com>
2024-09-30 22:38:03 +0800

    Add files via upload

---
+++ Posttest4/login.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="style.css" type="text/css">
+
+<div class="main">
+
+<h1 class="menu-login"> Log In Form </h1>
+
+<form action="" class="login-form" method="get">
+
+        <div class="login">
+           <label for="username" class="samain">Username</label>
+           <input type="text" name="username" id="username" required>
+         </div>
+      
+         <div class="login">
+           <label for="password" class="samain" >Password</label>
+           <input type="password"  name="password" id="password" required>
+         </div> 
+       
+       <div class="login">
+           <label for="Age"class="samain" >Age</label>
+           <input type="number" name="Age" id="Age" min="0" max="120" required>
+         </div> 
+        
+         <button class="login-button">
+           LOGIN
+           </button>
+         </form>
+</div>
